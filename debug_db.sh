1963bee65b362d8f7170a950338c7ff15e4b9aad
Marc Frances Torres <marc.frances.t@gmail.com>
2025-04-01 18:24:23 +0200

    Rename database from prueba to solidityhub_db

--- debug_db.sh
+++ debug_db.sh
@@ -1,5 +1,5 @@
 # Antes de esto ejecutar docker compose up -d
 
-docker exec -it solidarity_mysql mysql -u root -proot -D solidarityprueba
+docker exec -it solidarityhub_mysql mysql -u root -proot -D solidarityhub_db
 
 # Despues ejecutar docker compose down (cuando se quiera eliminar el contenedor)
