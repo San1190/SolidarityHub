9afcd74ab23b9a47978871329217e245afd0c27a
Marc Frances Torres <marc.frances.t@gmail.com>
2025-04-01 14:18:45 +0200

    Comando para acceder al contenedor mysql y poder ejecutar queries

---
+++ connect_db.sh
@@ -0,0 +1,5 @@
+# Antes de esto ejecutar docker compose up -d
+
+docker exec -it solidarity_mysql mysql -u root -proot -D solidarityprueba
+
+# Despues ejecutar docker compose down (cuando se quiera eliminar el contenedor)
