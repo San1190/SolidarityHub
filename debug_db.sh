1ea681727211497232aa8b17a6dcac467da3cd5c
Marc Frances <marc.frances.t@gmail.com>
2025-04-16 09:56:05 +0200

    Añadir dev docker compose para containerize toda la app

--- debug_db.sh
+++ debug_db.sh
@@ -1,5 +1,7 @@
 # Antes de esto ejecutar docker compose up -d
 
+docker exec -it solidarityhub_mysql-dev mysql -u root -proot -D solidarityhub_db
+
 docker exec -it solidarityhub_mysql mysql -u root -proot -D solidarityhub_db
 
 # Despues ejecutar docker compose down (cuando se quiera eliminar el contenedor)
